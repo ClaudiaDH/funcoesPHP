a77540945f7c59a757f48b9ca53e5243503023a8
ClaudiaDH <claudialeite.estudos@gmail.com>
2019-10-07 12:04:53 -0300

    testando funcoes

---
+++ teste.php
@@ -0,0 +1,63 @@
+<?php
+
+    //01.função com parâmetro.
+    // function tabuada($numero){
+
+    //     for($i=0; $i<= 10; $i++){
+
+    //         echo $numero."x".$i." = ".($numero * $i)."<br>";
+    //     }
+
+    // }
+
+    // tabuada(3);
+
+    //02.função sem parâmetro
+    //Sempre que meu cliente estiver logada, vai aparecer algo.
+    // function logada(){
+    //     echo "Seja bem vindo";
+    // }
+    // logada();
+
+
+    //03.função sem retorno
+    // //Receber relação de filmes assistidos e não assistidos.
+    //importante para que as ações sejam feitas, mas nem sempre o feedback do usuario é importante.
+    // function filmes($assistidos, $nãoAssistidos){
+    //     $total = $assistidos + $nãoAssistidos;
+    // }
+    // filmes(4,55);
+
+    //04.
+    // function soma($valorA, $valorB){
+    //     //vai retornar a soma.
+    //     return $valorA + $valorB;
+
+    // }
+    // echo soma(1,1);
+
+    // //05.usanod variavel global
+    // $valorA=10;
+    // $valorB=15;
+    // function soma(){
+    //     //vai retornar a soma.
+    //     //avis que são globais
+    //     global $valorA,$valorB;
+    //     //altera o valor da variavel
+    //     $valorA = $valorA + 1;
+    //     return $valorA + $valorB;
+    
+    // }
+    // echo soma()."<br>";
+    // echo soma()."<br>";
+    // echo $valorA;
+
+    //06.valor pre determinado
+    // function filmes($assistidos, $nãoAssistidos = 15){
+    //     return $assistidos + $nãoAssistidos;
+    // }
+    // echo filmes(4);
+
+    //07.Required e Include
+
+?>
